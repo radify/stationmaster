a75368b23ebc1b8a772d32b026ca8d6846087b6c
Gavin Davies <gavindavies@gmail.com>
2014-07-15 12:11:31 +0100

    Docs and UI enhancements
    * Ignore CSV file
    * Show a message if there are no results
    * Add documentation

--- public/js/services.js
+++ public/js/services.js
@@ -11,14 +11,17 @@ angular.module('stationmaster.services', [])
         var headers = lines[0].split(",");
 
         for(var i = 1; i < lines.length; i++) {
-            var obj = {};
-            var line = lines[i].split(",");
+          var obj = {};
+          var line = lines[i].split(",");
+          if (line.length < 2) {
+            continue;
+          }
 
-            for(var j = 0; j<headers.length; j++){
-                obj[headers[j]] = line[j];
-            }
+          for(var j = 0; j<headers.length; j++){
+            obj[headers[j]] = line[j];
+          }
 
-            result.push(obj);
+          result.push(obj);
         }
 
         return result;
