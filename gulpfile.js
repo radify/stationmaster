80e3b81967aa08d19540eca4d27181ebdf395829
John F. Mercer <john@radify.io>
2015-08-20 11:45:43 -0400

    Activates Tests
    * Previous to this commit, the tests didn't run at all. This was due to
      a misconfiguration of karma and jasmine.
    * Deletes gulp-jasmine and gulp-karma
        * As is [outlined here](https://github.com/karma-runner/gulp-karma),
          gulp does not require the now-dead gulp-karma plugin because gulp
          directly invokes karma's public API. Hence, I deleted the
          gulp-karma plugin.
        * jasmine is invoked via karma.js.conf, so there is no need for the
          gulp-jasmine plugin.
    * Greatly simplifies Gulpfile
        * Much of the test configuration has been moved to karma.js.conf.
        * Gulpfile now directly invokes karma, which is configured to use
          jasmine.
    * Replaces old gulp tasks "test" and "watch" with new ones.
    * NOTE: gulp lint, which is outside of the scope of this commit, throws
      errors galore. I will address that problem in a separate PR.
    
    * Run `gulp test`. **Assert that** it passes.
    * Run `gulp watch`.
      * **Assert that** it passes.
      * Break something. **Assert that** `gulp watch` reports the problem.
      * Fix it. **Assert that** `gulp watch` is happy again.

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,18 @@
-var gulp   = require('gulp');
-var jshint = require('gulp-jshint');
-var karma = require('gulp-karma');
-var jasmine = require('gulp-jasmine');
+var gulp     = require('gulp');
+var jshint   = require('gulp-jshint');
+var Server   = require('karma').Server;
 
-var testFiles = [
-  'public/bower_components/angular/angular.js',
-  'public/bower_components/angular-route/angular-route.js',
-  'public/bower_components/angular-mocks/angular-mocks.js',
-  'public/lib/*.js',
-  'public/js/*.js',
-  'spec/*.js'
-];
-
-// tun tests once
-gulp.task('test', function() {
-	return gulp.src(testFiles)
-		.pipe(karma({
-			configFile: 'conf/karma.conf.js',
-			action: 'run'
-		}))
-		.on('error', function(err) {
-			// failed tests cause gulp to exit non-zero
-			throw err;
-		})
-		.pipe(jasmine());
+gulp.task('test', function (done) {
+  new Server({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done).start();
 });
 
-// continuously run tests on change
-gulp.task('watch', function() {
-  gulp.src(testFiles)
-    .pipe(karma({
-        configFile: 'conf/karma.conf.js',
-        action: 'watch'
-    })).on('error', function(err) {
-        // failed tests cause gulp to exit non-zero
-        throw err;
-    })
-    .pipe(jasmine());
+gulp.task('watch', function (done) {
+  new Server({
+    configFile: __dirname + '/karma.conf.js'
+  }, done).start();
 });
 
 gulp.task('lint', function() {
