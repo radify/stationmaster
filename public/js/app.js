e4e70e93174f34cfcb0f51408b869f0d46fbaa08
John F. Mercer <john@radify.io>
2015-08-20 12:32:56 -0400

    Fixes gulp lint task
    * Previously, `gulp lint` produced many, many errors.
    * This commit wraps the following in IIFEs to eliminate the errors.
        * `public/js/app.js`
        * `public/js/controllers.js`
        * `public/js/filters.js`
        * `public/js/services.js`

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,14 @@
-'use strict';
+(function() {
+  'use strict';
 
-// Declare app level module which depends on filters, and services
-angular.module('stationmaster', [
-  'ngRoute',
-  'stationmaster.services',
-  'stationmaster.filters',
-  'stationmaster.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({templateUrl: 'partials/table.html', controller: 'TableController'});
-}]);
+  // Declare app level module which depends on filters, and services
+  angular.module('stationmaster', [
+    'ngRoute',
+    'stationmaster.services',
+    'stationmaster.filters',
+    'stationmaster.controllers'
+  ]).
+  config(['$routeProvider', function($routeProvider) {
+    $routeProvider.otherwise({templateUrl: 'partials/table.html', controller: 'TableController'});
+  }]);
+}());
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,22 +1,23 @@
-'use strict';
+(function() {
+  'use strict';
 
-angular.module('stationmaster.controllers', [])
-  .controller('TableController', ['$scope', '$http', 'CSVParser', function($scope, $http, CSVParser) {
-    var url = 'stationmaster.csv?cachebuster=' + Math.random();
-    angular.extend($scope, {
-      data: [],
-      loading: true,
-      loadCSV: function() {
-        $scope.loading = true;
-        $http.get(url).success(function(csv) {
-          $scope.data = CSVParser.parse(csv);
-        }).error(function() {
-          alert("File " + url + " is not found, please create it. You can use stationmaster-ansible to manage this CSV or do it through other means.");
-        }).finally(function() {
-          $scope.loading = false;
-        });
-      }
-    });
-
-    $scope.loadCSV();
-  }]);
+  angular.module('stationmaster.controllers', [])
+    .controller('TableController', ['$scope', '$http', 'CSVParser', function($scope, $http, CSVParser) {
+      var url = 'stationmaster.csv?cachebuster=' + Math.random();
+      angular.extend($scope, {
+        data: [],
+        loading: true,
+        loadCSV: function() {
+          $scope.loading = true;
+          $http.get(url).success(function(csv) {
+            $scope.data = CSVParser.parse(csv);
+          }).error(function() {
+            alert("File " + url + " is not found, please create it. You can use stationmaster-ansible to manage this CSV or do it through other means.");
+          }).finally(function() {
+            $scope.loading = false;
+          });
+        }
+      });
+      $scope.loadCSV();
+    }]);
+}());
